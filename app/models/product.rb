f5867719d74db014040eff2deaa36334312f9b70
Peter Jang <peter.x.jang@gmail.com>
2023-01-24 20:49:35 -0600

    Show supplier info with each product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 3..1000 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
   def is_discounted?
     price <= 10
   end
