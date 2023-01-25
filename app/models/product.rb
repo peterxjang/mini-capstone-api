8537cf39728a0b83d144bbacce657c8d56d8e372
Peter Jang <peter.x.jang@gmail.com>
2023-01-24 21:10:09 -0600

    Replace manual code with has_many / belongs_to shortcuts

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,10 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 3..1000 }
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
 
   def is_discounted?
     price <= 10
