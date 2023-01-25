425f210aa9672b9c4eea9567cbb0cd2265d95028
Peter Jang <peter.x.jang@gmail.com>
2023-01-24 20:18:16 -0600

    Add supplier model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_182155) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_021713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_182155) do
     t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
