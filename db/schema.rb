4f34cb6d5b08360855026598be8d93c6f3859df1
Peter Jang <peter.x.jang@gmail.com>
2023-01-24 20:25:56 -0600

    Add supplier id to product to create a has many / belongs to relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_021713) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_022332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_021713) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
