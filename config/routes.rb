1ff08b35692bfcf76bdb78fb402c49d89ba230d9
Peter Jang <peter.x.jang@gmail.com>
2023-01-26 21:05:06 -0600

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 
   get "/products" => "products#index"
   post "/products" => "products#create"
