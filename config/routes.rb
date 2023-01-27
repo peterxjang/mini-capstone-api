ac40c93acb72d9142f2a320f8f628ab0c09781e8
Peter Jang <peter.x.jang@gmail.com>
2023-01-26 21:03:54 -0600

    Add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/users" => "users#create"
+
   get "/products" => "products#index"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
