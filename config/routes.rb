3758a8b05c50252a2940646f390caecc09d0ab15
Peter Jang <peter.x.jang@gmail.com>
2023-01-29 11:47:00 -0600

    Add orders create action (simplified)

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  post "/orders" => "orders#create"
 end
