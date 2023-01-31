3634846740239005e7167526deed45c66861c2d5
Peter Jang <peter.x.jang@gmail.com>
2023-01-30 19:53:16 -0600

    Update products controller tests with authorization

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(name: "Admin", email: "admin@test.com", password: "password", admin: true)
+    post "/sessions.json", params: { email: "admin@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -19,18 +26,27 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { supplier_id: Supplier.first.id, price: 1, name: "test product", description: "test description", image_url: "image.jpg" }
+      post "/products.json",
+        headers: { "Authorization" => "Bearer #{@jwt}" },
+        params: { supplier_id: Supplier.first.id, price: 1, name: "test product", description: "test description", image_url: "image.jpg" }
       data = JSON.parse(response.body)
       assert_response 200
     end
 
-    post "/products.json", params: {}
+    post "/products.json",
+      headers: { "Authorization" => "Bearer #{@jwt}" },
+      params: {}
     assert_response 422
+
+    post "/products.json"
+    assert_response 401
   end
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json",
+      headers: { "Authorization" => "Bearer #{@jwt}" },
+      params: { name: "Updated name" }
     assert_response 200
 
     data = JSON.parse(response.body)
@@ -38,14 +54,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal product.price.to_s, data["price"]
     assert_equal product.description, data["description"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    patch "/products/#{product.id}.json",
+      headers: { "Authorization" => "Bearer #{@jwt}" },
+      params: { name: "" }
     assert_response 422
+
+    patch "/products/#{product.id}.json"
+    assert_response 401
   end
 
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json",
+        headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
+
+    delete "/products/#{Product.first.id}.json"
+    assert_response 401
   end
 end
