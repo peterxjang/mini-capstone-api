fc7f87f5c81f776691465b6d84c4fa31cf80d59f
Peter Jang <peter.x.jang@gmail.com>
2023-01-31 19:32:25 -0600

    Add has_many through relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ApplicationRecord
   has_many :category_products
+
+  has_many :products, through: :category_products
+  # def products
+  #   result = []
+  #   index = 0
+  #   while index < category_products.length
+  #     result << category_products[index].product
+  #     index += 1
+  #   end
+  #   result
+  # end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   def is_discounted?
     price <= 10
