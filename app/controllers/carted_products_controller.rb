752e491387ddf6252384e4ea1416a1fd1242ba90
Peter Jang <peter.x.jang@gmail.com>
2023-02-02 18:46:19 -0600

    Add carted_products index action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,9 @@
 class CartedProductsController < ApplicationController
+  def index
+    @carted_products = current_user.carted_products # alternatively: CartedProduct.where(user_id: current_user.id)
+    render :index
+  end
+
   def create
     @carted_product = CartedProduct.create(
       user_id: current_user.id,
