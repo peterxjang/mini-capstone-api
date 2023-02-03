f0c29f0e27f3fefb501b5eae630e15f760642e73
Peter Jang <peter.x.jang@gmail.com>
2023-02-02 20:31:22 -0600

    Refactor money calculations into model method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,31 +7,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-    calculated_subtotal = 0
-    index = 0
-    while index < carted_products.length
-      carted_product = carted_products[index]
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-      index += 1
-    end
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.create(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
     )
-
-    index = 0
-    while index < carted_products.length
-      carted_product = carted_products[index]
-      carted_product.update(status: "purchased", order_id: @order.id)
-      index += 1
-    end
-
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+    @order.update_totals
     render :show
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,17 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def update_totals
+    calculated_subtotal = 0
+    index = 0
+    while index < carted_products.length
+      carted_product = carted_products[index]
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+      index += 1
+    end
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+  end
 end
