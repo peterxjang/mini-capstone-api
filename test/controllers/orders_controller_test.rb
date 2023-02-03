41f35abf3cbfcdf88e127ec8956c847fa87e664e
Peter Jang <peter.x.jang@gmail.com>
2023-02-02 20:34:59 -0600

    Fix orders controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.create(name: "Test", email: "test@test.com", password: "password")
-    @order = Order.create(user_id: @user.id, product_id: Product.first.id, quantity: 10)
+    @order = Order.create(user_id: @user.id)
     post "/sessions.json", params: { email: "test@test.com", password: "password" }
     data = JSON.parse(response.body)
     @jwt = data["jwt"]
@@ -28,6 +28,6 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "quantity", "subtotal", "tax", "total", "product", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "subtotal", "tax", "total", "carted_products", "created_at", "updated_at"], data.keys
   end
 end
